3186611fedb53672b441015afaa85f269d456c5d
rope-park <kelly6789@naver.com>
2024-11-15 17:11:09 +0900

    Solve 백준 1475번 방 번호 by Python

--- Data Structure/1. Array/1475_RoomNumber_baekjoon.py
+++ Data Structure/1. Array/1475_RoomNumber_baekjoon.py
@@ -5,38 +5,39 @@
 '''
 
 '''
-zA, B, C는 모두 100보다 크거나 같고, 1,000보다 작은 자연수이다.
+입력
+- 첫째 줄에 다솜이의 방 번호 N이 주어진다. N은 1,000,000보다 작거나 같은 자연수이다.
 '''
 
 '''
 출력
-- 첫째 줄에는 A × B × C의 결과에 0 이 몇 번 쓰였는지 출력한다. 
-마찬가지로 둘째 줄부터 열 번째 줄까지 A × B × C의 결과에 1부터 9까지의 숫자가 각각 몇 번 쓰였는지 차례로 한 줄에 하나씩 출력한다.
+- 첫째 줄에 필요한 세트의 개수를 출력한다.
 '''
 
 # Ans.1
-def countNumbers(A, B, C):
+def roomNumber(N):
 
-    # 매개변수끼리 곱한 후 문자열로 변환
-    multiple_str = str(A * B * C)
+    N = list(str(N))
+    count = [0] * 10
 
-    # 숫자 개수를 세기 위한 리스트 초기화
-    count = [0] * 10 # 0 ~ 9까지의 숫자 개수 저장
-
-    # 각 숫자 개수 세기
-    for char in multiple_str:
-        index = ord(char) - ord('0')
+    for char in N:
+        index = int(char)
         count[index] += 1
 
-    return count
+    count[6] = (count[6] + count[9] + 1) // 2
+    count[9] = count[6]
+
+    max_count = max(count)
+
+    return max_count
+
 
 # 입력 처리
 if __name__ == "__main__":
-    n1 = int(input())
-    n2 = int(input())
-    n3 = int(input())
+    room_number = int(input())
 
-    result = countNumbers(n1, n2, n3) # 함수 호출
+    # 함수 호출
+    result = roomNumber(room_number)
 
     # 결과 출력
-    print('\n'.join(map(str, result))) # 줄바꿈으로 구분하여 출력
+    print(result)
